78532ebe8dd98f79f3e5faa58be63568fafe842a
Sambhav Kumar <kumarsambhav00@gmail.com>
2024-04-10 01:12:23 +0530

    final commit

---
+++ controllers/auth.js
@@ -0,0 +1,32 @@
+const User = require("../models/user");
+
+exports.getLogin = (req, res, next) => {
+  // const isLoggedIn = req.get("Cookie").trim().split("=")[1] === "true";
+  res.render("auth/login", {
+    path: "/login",
+    pageTitle: "Login",
+    isAuthenticated: false,
+  });
+};
+
+exports.postLogin = (req, res, next) => {
+  User.findById("65e96185c62b9c3be2fc58f1")
+    .then((user) => {
+      req.session.isLoggedIn = true;
+      req.session.user = user;
+      req.session.save((err) => {
+        console.log(err);
+        res.redirect("/");
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+exports.postLogout = (req, res, next) => {
+  req.session.destroy((err) => {
+    console.log(err);
+    res.redirect("/");
+  });
+};
